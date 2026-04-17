37f1e3dbf42bfc9b1a0a68f13009e40565324545
Sergio <sergiocdi@educastur.org>
2026-04-17 12:05:19 +0200

    feat: Mobile support with Capacitor (Android platform added)

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.velocity.tracker',
+  appName: 'Velocity',
+  webDir: 'dist/training-tracker/browser'
+};
+
+export default config;
